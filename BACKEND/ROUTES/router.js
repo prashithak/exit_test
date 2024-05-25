876a31828412b3b42cd7c18f752328825278292f
prashithak <135314888+prashithak@users.noreply.github.com>
2024-05-25 23:33:47 +0530

    commit

--- BACKEND/ROUTES/router.js
+++ BACKEND/ROUTES/router.js
@@ -4,5 +4,26 @@ const todo=require('../MODEL/todos')
 
 router.use(express.json());
 
+router.get('/todos', async (req, res) => {
+    
+       todo.find().then(data=>res.json(data))
+       .catch(err=>res.json(err))
+  });
 
+
+
+
+
+  router.post('/todos', async (req, res) => {
+    const todo = new todo({
+      des: req.body.des,
+      status: req.body.st || 'ongoing',
+    });
+    try {
+      const newTodo = await todo.save();
+      res.status(201).json(newTodo);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  });
 module.exports=router;
